298d506d2b9c60202ac77223f39ced59dc145fba
Haoheng Duan <duanhaoheng@gmail.com>
2021-07-15 11:37:27 +0800

    change attributes and add tests for ally and character

--- src/test/integrationTest.java
+++ src/test/integrationTest.java
@@ -1,4 +1,8 @@
 package test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
+import javafx.beans.property.SimpleIntegerProperty;
 
 public class integrationTest {
 
